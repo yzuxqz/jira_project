79cfa5450b6919777d53ab0b147fed9291088a5f
钱洲 许 <yzuxqz2020@gmail.com>
2021-02-04 21:15:02 +0800

    feat:添加useMount和useDebounce两个custonhook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -4,7 +4,7 @@ import * as qs from 'qs'
 import {SearchPanel} from "./search-panel";
 import {List} from "./list";
 import {useEffect, useState} from "react";
-import {cleanObject} from "../../utils";
+import {cleanObject,useDebounce,useMount} from "../../utils";
 
 export const ProjectListScreen=()=>{
   const [users, setUsers] = useState([])
@@ -12,23 +12,24 @@ export const ProjectListScreen=()=>{
     name: '',
     personId: ''
   })
+  const debounceParam= useDebounce(param,1000)
   const [list, setList] = useState([])
   //当用户输入关键词或选择select框，param变化
   useEffect(() => {
-    fetch(`http://localhost:3000/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`http://localhost:3000/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debounceParam])
 
-  useEffect(()=>{
+  useMount(()=>{
     fetch('http://localhost:3000/users').then(async response=>{
       if(response.ok){
         setUsers(await response.json())
       }
     })
-  },[])
+  })
   return <div>
     <SearchPanel param={param} users={users} setParam={setParam}/>
     <List list={list} users={users}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 //在一个函数中，改变传入的对象本身是不好的
 export const cleanObject = (object) => {
@@ -10,4 +12,23 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback;
+  }, []);
+};
+export const useDebounce = (value, delay) => {
+  const [newValue, setNewValue] = useState(value);
+  useEffect(() => {
+    //每次在value变化以后，设置一个定时器
+    const timer = setTimeout(() => {
+      setNewValue(value);
+    }, 2000);
+    //每次在上一个useEffect处理完以后再运行，第一个effect的timer被第二个effect清理，最后一个无人清理
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+  return newValue;
 };
